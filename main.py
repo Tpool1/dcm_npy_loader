3af0f8b362637d15fe0a0d964dee1273f5590ae6
Tpool1 <tristenpool89@gmail.com>
2021-07-01 18:23:01 -0500

    adjust percent

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ for (dirpath, dirnames, filenames) in os.walk(load_dir):
     load_paths += [os.path.join(dirpath, file) for file in filenames]
 
 # get random subset of list
-percent_sample = 25
+percent_sample = 50
 total_imgs = len(load_paths) * (percent_sample*0.01)
 
 # round down
