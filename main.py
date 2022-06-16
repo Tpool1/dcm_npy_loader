1d61e4cee0ec50e45d9212498125a9cc34536774
Tristen Pool <tristenpool89@gmail.com>
2022-06-16 18:21:28 -0500

    pickle final list

--- main.py
+++ main.py
@@ -1,97 +1,117 @@
 import torchio
 import os
 import numpy as np
-import pydicom as dicom
+import pydicom
 import time
 import torch
 import random
 import math
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import pickle
 
-def dcm_npy_loader(load_dir, imgs_shape=(512, 512), crop_shape=(512, 512)):
-    prev_time = time.time()
-    os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+def dcm_npy_loader(load_dir, shape=(512, 512), load=True):
+    if not load:
+        prev_time = time.time()
+        os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-    load_paths = list()
-    for (dirpath, dirnames, filenames) in os.walk(load_dir):
-        load_paths += [os.path.join(dirpath, file) for file in filenames]
+        load_paths = list()
+        for (dirpath, dirnames, filenames) in os.walk(load_dir):
+            load_paths += [os.path.join(dirpath, file) for file in filenames]
 
-    # get random subset of list
-    percent_sample = 100
-    total_imgs = len(load_paths) * (percent_sample*0.01)
+        # get random subset of list
+        percent_sample = 100
+        total_imgs = len(load_paths) * (percent_sample*0.01)
 
-    # round down
-    total_imgs = math.floor(total_imgs)
+        # round down
+        total_imgs = math.floor(total_imgs)
 
-    new_path_list = []
-    i = 0
-    while i < total_imgs:
-        rand_choice = random.choice(load_paths)
+        new_path_list = []
+        i = 0
+        while i < total_imgs:
+            rand_choice = random.choice(load_paths)
 
-        if rand_choice not in new_path_list:
-            new_path_list.append(rand_choice)
-            i = i + 1
+            if rand_choice not in new_path_list:
+                new_path_list.append(rand_choice)
+                i = i + 1
 
-    load_paths = new_path_list
+        load_paths = new_path_list
 
-    img_list = []
-    for path in load_paths:
+        files = []
+        ids = []
+        for path in load_paths:
+            try:
+                file = pydicom.dcmread(path)
+                if file.pixel_array.shape == shape:
+                    files.append(file)
+                    id = file.PatientID
+                    ids.append(id)
+            except:
+                print("Image " + path + " could not be loaded")
 
-        try: 
-            img = dicom.dcmread(path)
-            img_shape = img.pixel_array.shape
-            id = img.PatientID
-            slice_location = img.get('SliceLocation')
+        # remove duplicates
+        ids = list(set(ids))
 
-            if img_shape == imgs_shape:
-                for c in id:
-                    if not c.isdigit():
-                        id = id.replace(c, '')
+        all_img3d = []
+        all_ids = []
+        all_sliceLocs = []
+        for id in ids:
+        
+            # skip files with no SliceLocation
+            slices = []
+            skipcount = 0
+            for f in files:
+                if f.PatientID == id:
+                    if hasattr(f, 'SliceLocation'):
+                        slices.append(f)
+                    else:
+                        skipcount = skipcount + 1
+                
+            # ensure slices are in the correct order
+            slices = sorted(slices, key=lambda s: s.SliceLocation)
 
-                subject_dict = {
-                    'one image': torchio.ScalarImage(path),
-                    'id': id,
-                    'SliceLocation': slice_location
-                }
+            # pixel aspects, assuming all slices are the same
+            ps = slices[0].PixelSpacing
+            ss = slices[0].SliceThickness
+            ax_aspect = ps[1]/ps[0]
+            sag_aspect = ps[1]/ss
+            cor_aspect = ss/ps[0]
 
-                subject = torchio.Subject(subject_dict)
+            # create 3D array
+            img_shape = list(slices[0].pixel_array.shape)
+            img_shape.append(len(slices))
+            img3d = np.zeros(img_shape, dtype=np.int8)
 
-                img_list.append(subject)
+            p_id = slices[0].PatientID
+            # get only numbers from patient id
+            p_id = [int(s) for s in p_id if s.isdigit()]
+            p_id = int(''.join([str(i) for i in p_id]))
 
-        except:
-            print('image ' + str(path) + ' could not be loaded')
+            slice_locs = []
 
-    dataset = torchio.SubjectsDataset(img_list)
-    print('Total length of dataset: ' + str(len(dataset)))
+            # fill 3D array with the images from the files
+            for i, s in enumerate(slices):
+                img2d = s.pixel_array
 
-    # +2 for slice location and id at end
-    img_array = np.empty(shape=(len(dataset), (crop_shape[0]*crop_shape[1])+2), dtype=np.float16)
+                slice_locs.append(s.get('SliceLocation'))
+                if list(img2d.shape) == img_shape[:2]:
+                    img3d[:, :, i] = img2d
+            
+            all_ids.append(p_id)
+            all_img3d.append(img3d)
+            all_sliceLocs.append(slice_locs)
 
-    for i in range(len(dataset)):
+        after_time = time.time()
+        load_time = after_time - prev_time
+        print(load_time)
 
-        loader = torch.utils.data.DataLoader(dataset, shuffle=True)
+        data = [all_img3d, all_ids, all_sliceLocs]
 
-        id = int(next(iter(loader))['id'][0])
-        slice_location = float(next(iter(loader))['SliceLocation'])
-        image = next(iter(loader))['one image']['data']
+        with open('data\\Duke-Breast-Cancer-MRI\\data', 'wb') as fp:
+            pickle.dump(data, fp)
 
-        image = image.numpy()
+    else:
+        with open('data\\Duke-Breast-Cancer-MRI\\data', 'rb') as fp:
+            data = pickle.load(fp)
 
-        # crop image
-        image = tf.image.random_crop(value=image, size=(1, 1, crop_shape[0], crop_shape[1], 1))
-
-        image = image.numpy()
-
-        image = image.flatten()
-
-        image = np.append(image, slice_location)
-        image = np.append(image, id)
-
-        img_array[i] = image
-
-    np.save('img_array.npy', img_array)
-
-    after_time = time.time()
-
-    load_time = after_time - prev_time
-    print(load_time)
+    return data
